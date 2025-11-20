9d695ccf9093fd813cec13f0681262667dc3c618
Thunder <thunder@disroot.org>
2025-11-20 15:56:26 +0200

    make test buildable

--- config/test.nix
+++ config/test.nix
@@ -3,5 +3,14 @@
   config.brainrotos = {
     desktop.plasma.v1 = true;
     impermanence.enable.v1 = true;
+    efi.v1 = true;
+  };
+
+  nixpkgs.hostPlatform = {system = "x86_64-linux";};
+
+  users.users.test = {
+    isNormalUser = true;
+    extraGroups = ["wheel"];
+    initialPassword = "password";
   };
 }
