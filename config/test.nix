d469612d29932b4a58bb161d1f774146c6b4a23d
Thunder <thunder@disroot.org>
2025-11-20 16:43:10 +0200

    fix

--- config/test.nix
+++ config/test.nix
@@ -5,7 +5,7 @@
       desktop.plasma.v1 = true;
       impermanence.enable.v1 = true;
       efi.v1 = true;
-      ramcache.v1 = true;
+      ramcache.enable.v1 = true;
     };
 
     nixpkgs.hostPlatform = {system = "x86_64-linux";};
