731bb34100a95040327be2e474c976e22a2a6419
Thunder <thunder@disroot.org>
2025-11-20 17:19:03 +0200

    kernel modules to load disks lol

--- modules/nixos/base.nix
+++ modules/nixos/base.nix
@@ -4,5 +4,15 @@
 
     boot.initrd.systemd.enable = true;
     boot.initrd.systemd.emergencyAccess = true;
+
+    boot.initrd.availableKernelModules = [
+      "ahci"
+      "virtio_pci"
+      "virtio_scsi"
+      "virtio_blk"
+      "scsi_mod"
+      "sd_mod"
+      "nvme"
+    ];
   };
 }
