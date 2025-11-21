4346b05f8725a752f5e1dcd9dadc336c54e82347
Thunder <thunder@disroot.org>
2025-11-21 11:36:11 +0200

    disable fail check it doesn't work

--- config/base.nix
+++ config/base.nix
@@ -11,6 +11,6 @@ in {
       flatpak.v1.enable = mkDefault true;
     };
 
-    systemd.services.display-manager.serviceConfig.ExecStart = lib.mkForce "/run/current-system/sw/bin/false";
+    # systemd.services.display-manager.serviceConfig.ExecStart = lib.mkForce "/run/current-system/sw/bin/false";
   };
 }
