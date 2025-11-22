b0a72c159740fe52d8712db9213c16554df12be1
Thunder <thunder@disroot.org>
2025-11-22 12:15:37 +0200

    wip boot validation for systemd boot

---
+++ modules/nixos/boot/default.nix
@@ -0,0 +1,63 @@
+{
+  lib,
+  config,
+  ...
+}: let
+  inherit (lib) mkDefault mkIf mkMerge;
+  inherit (lib.options) mkOption;
+  inherit (lib.types) bool enum;
+
+  cfg = config.brainrotos.boot.v1;
+in {
+  options = {
+    brainrotos.boot.v1 = {
+      enable = mkOption {
+        type = bool;
+        default = true;
+        description = "Configure bootloader and boot validation.";
+      };
+
+      efi = mkOption {
+        type = bool;
+        default = false;
+        description = "Does this system support EFI?";
+      };
+
+      bootloader = mkOption {
+        type = enum ["grub" "systemd"];
+        default =
+          if cfg.efi
+          then "systemd"
+          else "grub";
+        description = "Bootloader variant to install.";
+      };
+    };
+  };
+
+  imports = [
+    ./systemd-boot.nix
+  ];
+
+  config = mkMerge [
+    (mkIf (cfg.enable && cfg.efi) {
+      boot.loader.efi.canTouchEfiVariables = mkDefault true;
+      boot.loader.efi.efiSysMountPoint = mkDefault "/boot";
+    })
+    (mkIf (cfg.enable) {
+      systemd.services.brainrotos-validate-boot = {
+        enable = true;
+        description = "Validate system health and bless the newly booted configuration";
+
+        requires = ["display-manager.service" "local-fs.target"];
+        after = ["display-manger.service" "local-fs.target"];
+
+        wantedBy = ["multi-user.target"];
+
+        serviceConfig = {
+          Type = "oneshot";
+          User = "root";
+        };
+      };
+    })
+  ];
+}
---
+++ modules/nixos/boot/systemd-boot.nix
@@ -0,0 +1,71 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}: let
+  inherit (lib) mkIf mkMerge getExe' makeBinPath;
+
+  cfg = config.brainrotos.boot.v1;
+in {
+  config = mkMerge [
+    (mkIf (cfg.enable && cfg.efi && cfg.bootloader == "systemd") {
+      boot.loader.systemd-boot = {
+        enable = true;
+        editor = false;
+
+        extraInstallCommands = let
+          path = makeBinPath [
+            pkgs.jq
+            pkgs.systemd
+          ];
+        in ''
+          PATH=${path}:$PATH
+
+          json=$(bootctl list --json=short)
+
+          new_id=$(echo "$json" | jq -r '.[0].id')
+          previous_id=$(echo "$json" | jq -r '.[1].id')
+
+          # stable id won't exist on a fresh install
+          if [ "$stable_id" != "null" ] && [ -n "$stable_id" ]; then
+            echo "  Boot validation in effect"
+            echo "  New generation: $new_id"
+            echo "  Old generation: $previous_id"
+
+            # default to old one
+            bootctl set-default "$previous_id"
+            # boot new one once
+            bootctl set-oneshot "$new_id"
+          else
+            echo "  No previous generation detected. Defaulting to new generation."
+          fi
+        '';
+      };
+
+      systemd.services.brainrotos-validate-boot = {
+        path = [
+          pkgs.systemd
+          pkgs.jq
+          pkgs.gawk
+        ];
+
+        script = ''
+          newest=$(bootctl list --json=short | jq '.[0].id')
+          current=$(bootctl list --json=short | jq 'first(.[] | select(.isSelected)).id')
+          default=$(bootctl list --json=short | jq 'first(.[] | select(.isDefault)).id')
+
+          if [ ! -n "$newest" ] || [ ! -n "$current" ] || [ ! -n "$default" ]; then
+            exit 1
+          fi
+
+          if [ "$newest" = "$current" ] && [ "$current" != "$default" ]; then
+            echo "Current generation validated successfully, setting as default"
+            bootctl set-default "$current"
+            echo "systemd-boot: Default entry updated to $current"
+          fi
+        '';
+      };
+    })
+  ];
+}
