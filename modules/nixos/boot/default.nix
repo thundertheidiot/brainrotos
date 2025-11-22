97e659541c2c977d9a44fbf4dbef3ee62b802429
Thunder <thunder@disroot.org>
2025-11-22 12:45:31 +0200

    add some waiting

--- modules/nixos/boot/default.nix
+++ modules/nixos/boot/default.nix
@@ -57,7 +57,19 @@ in {
         serviceConfig = {
           Type = "oneshot";
           User = "root";
-          ExecStartPre = map (target: "${getExe' pkgs.systemd "systemctl"} is-active --quiet ${target}") requires;
+          ExecStartPre =
+            map (
+              # give some time for the units to start up
+              target: ''
+                for i in 1 2 3 4 5; do
+                    ${getExe' pkgs.systemd "systemctl"} is-active --quiet ${target} && exit 0
+                    sleep 1
+                done
+                echo "${target} is not active"
+                exit 1
+              ''
+            )
+            after;
         };
       };
     })
