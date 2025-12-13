cf0b7c258457a07ad8ec0fef71a2cef6e45590dc
Thunder <thunder@disroot.org>
2025-12-13 17:42:23 +0200

    remove broken boot validation

--- modules/nixos/boot.nix
+++ modules/nixos/boot.nix
@@ -4,7 +4,7 @@
   pkgs,
   ...
 }: let
-  inherit (lib) mkDefault mkIf mkMerge getExe';
+  inherit (lib) mkDefault mkIf mkMerge;
   inherit (lib.options) mkOption;
   inherit (lib.types) bool;
 
@@ -14,7 +14,7 @@ in {
     brainrotos.efi.v1.enable = mkOption {
       type = bool;
       default = true;
-      description = "Enable EFI support (required for auto rollback).";
+      description = "Enable EFI support.";
     };
   };
 
@@ -33,56 +33,6 @@ in {
 
         efi.canTouchEfiVariables = mkDefault true;
         efi.efiSysMountPoint = mkDefault "/boot";
-      };
-    })
-    # boot counting
-    (mkIf cfg.enable {
-      # full disclaimer, made with the help of gemini 3 pro
-      boot.loader.systemd-boot.extraInstallCommands = let
-        esp = config.boot.loader.efi.efiSysMountPoint;
-        # consider switching these
-        grep = getExe' pkgs.busybox "grep";
-        awk = getExe' pkgs.busybox "awk";
-        mv = getExe' pkgs.busybox "mv";
-        sed = getExe' pkgs.busybox "sed";
-      in ''
-        DEFAULT_ENTRY=$(${grep} "^default" ${esp}/loader/loader.conf | ${awk} '{print $2}')
-
-        if [ -n "$DEFAULT_ENTRY" ] && [[ "$DEFAULT_ENTRY" != *"+"* ]]; then
-          TRIES=2
-
-          NEW_ENTRY="''${DEFAULT_ENTRY%.conf}+$TRIES.conf"
-
-          if [ -f "${esp}/loader/entries/$DEFAULT_ENTRY" ]; then
-            ${mv} "${esp}/loader/entries/$DEFAULT_ENTRY" "${esp}/loader/entries/$NEW_ENTRY"
-
-            # Update loader.conf to point to the new filename
-            ${sed} -i "s/$DEFAULT_ENTRY/$NEW_ENTRY/" "${esp}/loader/loader.conf"
-
-            echo "Boot counting enabled: Renamed $DEFAULT_ENTRY to $NEW_ENTRY"
-          fi
-        fi
-      '';
-
-      # https://blog.printk.io/2020/02/systemd-boot-counting-and-boot-complete-target/
-      systemd.targets."boot-complete" = {
-        enable = true;
-        wantedBy = ["basic.target"];
-      };
-
-      systemd.services."systemd-bless-boot".enable = false;
-      systemd.services."brainrotos-bless-boot" = {
-        enable = true;
-        wantedBy = ["multi-user.target"];
-        requires = ["boot-complete.target" "display-manager.service"];
-        after = ["local-fs.target" "boot-complete.target" "display-manager.service"];
-
-        serviceConfig = {
-          Type = "oneshot";
-          RemainAfterExit = true;
-
-          ExecStart = "${pkgs.systemd}/lib/systemd/systemd-bless-boot good";
-        };
       };
     })
   ];
