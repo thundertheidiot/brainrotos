0a3a07fb7600f6bfb587d7f2f3c557a9f6cd1d54
Thunder <thunder@disroot.org>
2025-12-30 18:10:38 +0200

    copy monitor config

--- modules/nixos/desktop/gnome.nix
+++ modules/nixos/desktop/gnome.nix
@@ -23,7 +23,6 @@ in {
   config = mkMerge [
     (mkIf cfg.enable {
       services.desktopManager.gnome.enable = true;
-      services.displayManager.gdm.enable = true;
       services.gnome.games.enable = false;
       services.gnome.core-developer-tools.enable = false;
 
@@ -32,14 +31,25 @@ in {
       services.gnome.gnome-software.enable = mkForce false;
     })
     (mkIf cfg.enable {
-      # impermanence
+      services.displayManager.gdm.enable = true;
+
+      systemd.services."display-manager".serviceConfig = {
+        # - here should ignore failure, which will happpen on first boot
+        ExecStartPre = "-${pkgs.writeShellScript "gdm-copy-monitor-config" ''
+          mkdir --parents /etc/xdg
+          cp "${config.users.users."${config.brainrotos.user.v1.name}".home}/.config/monitors.xml" /etc/xdg/monitors.xml
+        ''}";
+      };
+
+      # gdm persistence
       brainrotos.impermanence.v1.directories = [
         {
           path = "/var/lib/gdm";
           permissions = "755";
         }
       ];
-
+    })
+    (mkIf cfg.enable {
       # cache components to ram on boot
       brainrotos.ramcache.v1.paths = with pkgs; [
         nautilus
