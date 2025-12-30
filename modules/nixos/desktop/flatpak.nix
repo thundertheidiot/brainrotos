904ec80d374cd85f9f15082a636de0adc4f4a0af
thunder <58880811+thundertheidiot@users.noreply.github.com>
2025-12-30 17:21:08 +0200

    Bazaar app store installation and configuration (#1)

--- modules/nixos/desktop/flatpak.nix
+++ modules/nixos/desktop/flatpak.nix
@@ -42,26 +42,33 @@ in {
         "R! /var/tmp/flatpak-cache-* - - - 10d"
       ];
     })
+    # default flatpaks
     (mkIf cfg.enable {
       services.flatpak.packages = [
         "io.github.kolunmi.Bazaar"
+        "com.github.tchx84.Flatseal"
       ];
 
       services.flatpak.overrides."io.github.kolunmi.Bazaar" = {
         Context.filesystems = [
-          "host-os:ro" # expose bazaar configs
+          "host-etc:ro" # expose bazaar configs
         ];
       };
 
-      environment.etc."bazaar/bazaar.yaml".source = (pkgs.formats.yaml {}).generate "bazaar.yaml" {
-        txt-blocklist-paths = [
-          "/run/host/etc/bazaar/blocklist.txt"
-        ];
-      };
-
-      environment.etc."bazaar/blocklist.txt".text = ''
-        com.valvesoftware.Steam
-      '';
+      # environment.etc won't work here, because it actually links stuff into /etc/static
+      # and we cannot pass directories in /etc to the flatpak
+      # using C here avoids having to mount /nix/store for the symlinks
+      systemd.tmpfiles.rules = [
+        "d /etc/bazaar 0755 root root - -"
+        "C /etc/bazaar/bazaar.yaml - - - - ${(pkgs.formats.yaml {}).generate "bazaar.yaml" {
+          txt-blocklist-paths = [
+            "/run/host/etc/bazaar/blocklist.txt"
+          ];
+        }}"
+        "C /etc/bazaar/blocklist.txt - - - - ${pkgs.writeText "blocklist.txt" ''
+          com.valvesoftware.Steam
+        ''}"
+      ];
     })
     (mkIf (cfg.enable && config.brainrotos.desktop.plasma.v1.enable) {
       environment.systemPackages = [
